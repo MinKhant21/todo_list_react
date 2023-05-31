99a9e6ddd9488b445626633160b7db578620c887
minkhant21 <kaungminkhant6889@gmail.com>
2023-05-31 09:23:04 +0630

    u

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React, { useState } from 'react';
-import Card from './components/Card';
-import 'bootstrap/dist/css/bootstrap.css'
+import react, { useReducer, useState } from "react";
 
-
-function App(props) {
-    // const [counter , setCounter] = useState(0)
-    return (
-        <div>
-            <Card/>   
-        </div>
-        // <div>
-        //     <p>
-        //         {counter}
-        //     </p>
-        //     <button onClick={()=>{
-        //         setCounter(counter+1)
-        //     }}>Couter</button>
-        // </div>
-    );
+const reducer = (state,action) =>{
+    switch (action.type) {
+        case "INCREMENT":
+            return 
+            break;
+    
+        default:
+            break;
+    }
 }
-
-export default App;
+const App = (props) => {
+    // const [counter,setcounter] = useState(0)
+    // const [showText,setshowText] = useState(true)
+    const [state,dispatch] = useReducer(reducer,{counter:0,showText:true})
+    const  hello = () =>{
+        setcounter(counter+1)
+        setshowText(!showText)
+    }
+    return (
+        <>
+            {counter}
+            <button onClick={hello}>Click</button>
+            <p>
+                {showText ? 'this is text' : ""}
+            </p>
+        </>
+    )
+}
+export default App
